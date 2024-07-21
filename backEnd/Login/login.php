4e62b3ee48c6ec473ec4813a2a83a8c78e0d6ce4
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-07-21 17:14:52 +0530

    create login page

---
+++ backEnd/Login/login.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once '../DbConnection/db.conn.php';
+
+
+if(isset($_POST['login'])){
+    
+    $email = $_POST['email'];
+    $Password = $_POST['password'];
+
+    $sql_emailCheck = "SELECT * FROM `users` WHERE email = '$email' ";
+    $result_emailCheck = $conn->query($sql_emailCheck);
+
+    if($result_emailCheck->num_rows > 0){
+
+        $row = $result_emailCheck->fetch_ASSOC();
+
+        if($Password == $row['password']){
+
+            $_SESSION["loginSuccess"] = 1;
+            header("Location: ../../index.html"); 
+
+        }else{
+            $_SESSION["SignUpError"] = 1;
+            header("Location: ../../Login/login.html"); 
+        }
+
+
+        $_SESSION["SignUpError"] = 1;
+        header("Location: ../../index.html"); 
+
+    }else{
+        $_SESSION["SignUpError"] = 1;
+        header("Location: ../../index.html"); 
+    }
+
+}else if(isset($_POST['signup'])){
+    
+}
+
+
+
+
+?>
